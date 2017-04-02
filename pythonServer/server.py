1964e785097acf629114b6bb0c1e3ba8ae8c43c5
Angelica <angelica.perez37@gmail.com>
2017-04-02 12:57:27 -0700

    Updated messages being sent to RasPi

--- pythonServer/server.py
+++ pythonServer/server.py
@@ -5,7 +5,7 @@
 import socket
 import sys
 
-HOST = 'localhost'   # Symbolic name, meaning all available interfaces
+HOST = ''   # Symbolic name, meaning all available interfaces
 PORT = 8080 # Arbitrary non-privileged port
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
