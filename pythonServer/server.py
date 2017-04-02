24a1e7e50b547f83d5db566f23c12d2e28a39e62
Angelica <angelica.perez37@gmail.com>
2017-04-02 15:23:09 -0700

    Started RasPi Server function swing to execute swing

--- pythonServer/server.py
+++ pythonServer/server.py
@@ -8,41 +8,43 @@ import sys
 def swing():
     return "swing function completed"
 
-HOST = ''   # Symbolic name, meaning all available interfaces
-PORT = 8080 # Arbitrary non-privileged port
+def main(args):
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print 'Socket created'
+    HOST = ''   # Symbolic name, meaning all available interfaces
+    PORT = 8080 # Arbitrary non-privileged port
 
-#Bind socket to local host and port
-try:
-    s.bind((HOST, PORT))
-except socket.error as msg:
-    print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
-    sys.exit()
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print 'Socket created'
 
-print 'Socket bind complete'
+    #Bind socket to local host and port
+    try:
+        s.bind((HOST, PORT))
+    except socket.error as msg:
+        print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+        sys.exit()
 
-#Start listening on socket
-s.listen(8080)
-print 'Socket now listening'
+    print 'Socket bind complete'
 
-#now keep talking with the client
-while 1:
-    #wait to accept a connection - blocking call
-    conn, addr = s.accept()
-    print 'Connected with ' + addr[0] + ':' + str(addr[1])
-    #conn.send("Hello")
-    data = conn.recv(8080)
-    if data == 'q' or data == 'Q':
-        s.close()
-        break
-    elif data == 'swing':
-        print 'Received: ' + data
-        output = swing()
-        conn.send(output)
-        s.close()
+    #Start listening on socket
+    s.listen(8080)
+    print 'Socket now listening'
 
+    #now keep talking with the client
+    while 1:
+        #wait to accept a connection - blocking call
+        conn, addr = s.accept()
+        print 'Connected with ' + addr[0] + ':' + str(addr[1])
+        #conn.send("Hello")
+        data = conn.recv(8080)
+        if data == 'q' or data == 'Q':
+            s.close()
+            break
+        elif data == 'swing':
+            print 'Received: ' + data
+            output = swing()
+            conn.send(output)
+            s.close()
 
+    s.close()
 
-s.close()
+main()
