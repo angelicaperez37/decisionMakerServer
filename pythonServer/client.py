5db5ddd1cf3fcd8a216e0b4ffa055e5324ccb4ae
Angelica <angelica.perez37@gmail.com>
2017-04-02 12:56:09 -0700

    Updated messages being sent to RasPi

--- pythonServer/client.py
+++ pythonServer/client.py
@@ -1,6 +1,6 @@
 import socket              # Import socket module
 
-s = socket.socket()        # Create a socket object
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)        # Create a socket object
 port = 8080                # Reserve a port for your service.
 
 s.connect(("192.168.1.85", port))
