c893376cd104f24935e36b5ae77c6f7a82ed2798
Angelica <angelica.perez37@gmail.com>
2017-04-02 15:50:34 -0700

    Cleaned up client.py

--- pythonServer/client.py
+++ pythonServer/client.py
@@ -2,23 +2,26 @@ import socket              # Import socket module
 import sys
 import os
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)        # Create a socket object
-port = 8080                # Reserve a port for your service.
+def main(args):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)        # Create a socket object
+    port = 8080                # Reserve a port for your service.
 
-s.connect(("192.168.1.85", port))
-#s.send("swing")
-#while(1):
-s.sendall("swing")
+    s.connect(("192.168.1.85", port))
+    #s.send("swing")
+    #while(1):
+    s.sendall("swing")
 
-f = open('img.jpg','wb')
+    f = open('img.jpg','wb')
 
-print "Receiving..."
-l = s.recv(1024)
-while (l):
     print "Receiving..."
-    f.write(l)
     l = s.recv(1024)
-f.close()
-print "Done Receiving"
+    while (l):
+        print "Receiving..."
+        f.write(l)
+        l = s.recv(1024)
+    f.close()
+    print "Done Receiving"
 
-s.close                    # Close the socket when done
+    s.close                    # Close the socket when done
+
+main([])
